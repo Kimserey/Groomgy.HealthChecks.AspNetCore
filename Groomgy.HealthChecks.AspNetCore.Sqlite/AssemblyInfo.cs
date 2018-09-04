1fb2b8a66fa494ffaa39f6a828fd728aa0fba563
Kimserey Lam <Kimserey@users.noreply.github.com>
2018-09-04 22:52:41 +0100

    Added sqlite health check (#2)

---
+++ Groomgy.HealthChecks.AspNetCore.Sqlite/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+[assembly: System.Reflection.AssemblyCompanyAttribute("Kimserey Lam")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Sqlite Healh Check extension to be used with Groomgy Health Check library for ASP.NET Core")]
+[assembly: System.Reflection.AssemblyProductAttribute("Groomgy.HealthChecks.AspNetCore.Sqlite")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Groomgy.HealthChecks.AspNetCore.Sqlite")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0")]
---
+++ Groomgy.HealthChecks.AspNetCore.Sqlite/HealthCheckBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Groomgy.HealthChecks.AspNetCore.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Groomgy.HealthChecks.AspNetCore
+{
+    public static class HealthCheckBuilderExtensions
+    {
+        /// <summary>
+        /// Checks if the tables specified are accessible on the database using the connection string given.
+        /// </summary>
+        /// <param name="builder">Health check builder</param>
+        /// <param name="connectionString">Connection string to the Sqlite database</param>
+        /// <param name="tables">Tables to check for existence</param>
+        /// <returns></returns>
+        public static IHealthCheckBuilder AddSqliteCheck(this IHealthCheckBuilder builder, string connectionString, params string[] tables) =>
+            builder.Add(sp => new SqliteCheck(connectionString, tables, sp.GetRequiredService<ILogger<SqliteCheck>>()));
+    }
+}
