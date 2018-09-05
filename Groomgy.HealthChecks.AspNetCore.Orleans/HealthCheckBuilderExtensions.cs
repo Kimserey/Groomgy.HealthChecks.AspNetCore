2fcd385f3caa86863e1da90490773ff52060622b
Kimserey Lam <Kimserey@users.noreply.github.com>
2018-09-05 08:55:54 +0100

    Healthcheck checker (#3)
    * Remove folder from csproj
    
    * Added NuGet deployment
    
    * Added Orleans check
    
    * Assembly info for Orleans check

---
+++ Groomgy.HealthChecks.AspNetCore.Orleans/HealthCheckBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Groomgy.HealthChecks.AspNetCore.Orleans;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans;
+
+namespace Groomgy.HealthChecks.AspNetCore
+{
+    public static class HealthCheckBuilderExtensions
+    {
+        public static IHealthCheckBuilder AddOrleansClientCheck(this IHealthCheckBuilder builder, string clusterId) =>
+            builder.Add(sp => new OrleansClientCheck(() => sp.GetRequiredService<IGrainFactory>(), clusterId));
+    }
+}
