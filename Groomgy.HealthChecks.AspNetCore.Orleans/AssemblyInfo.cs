707b9a1183041f0b107b7578d86fe31023053121
Kimserey <kimserey.lam@groomgy.com>
2018-09-05 08:47:51 +0100

    Assembly info for Orleans check

---
+++ Groomgy.HealthChecks.AspNetCore.Orleans/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+[assembly: System.Reflection.AssemblyCompanyAttribute("Kimserey Lam")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Microsoft.Orleans Healh Check extension to be used with Groomgy Health Check library for ASP.NET Core")]
+[assembly: System.Reflection.AssemblyProductAttribute("Groomgy.HealthChecks.AspNetCore.Orleans")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Groomgy.HealthChecks.AspNetCore.Orleans")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0")]
